79f8693af846f61887938059f6135c5ab0e080c3
Maksym Chyshko <76655891+Maks2090@users.noreply.github.com>
2021-07-07 22:10:34 +0300

    home work

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
 import './sass/main.scss';
+
+// new CountdownTimer({
+//     selector: '#timer-1',
+//     targetDate: new Date('Jul 17, 2022'),
+//   });
+
+
+// const timer1 = document.querySelector('.timer')
+
+
+
+
+// function getTimeComponents(time){
+
+//   const days = Math.floor(time / (1000 * 60 * 60 * 24));
+//   const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+//   const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+//   return {days, hours, mins, secs}
+// }
+
+function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+    return { days, hours, minutes, seconds };
+  }
+  
+  console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+  console.log(convertMs(140000)); //{days: 0, hours: 0, minutes: 2, seconds: 20}
+  console.log(convertMs(24140000)); // {days: 0, hours: 6, minutes: 42, seconds: 20}
