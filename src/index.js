2c5941609e59c15fbb51500be8fbbd3310238e82
Maksym Chyshko <76655891+Maks2090@users.noreply.github.com>
2021-07-08 23:23:53 +0300

    home

--- src/index.js
+++ src/index.js
@@ -1,45 +1,61 @@
 import './sass/main.scss';
 
-// new CountdownTimer({
-//     selector: '#timer-1',
-//     targetDate: new Date('Jul 17, 2022'),
-//   });
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+      this.selector = document.querySelector(`${selector}`);
+      this.targetDate = targetDate;
+      this.timer();
+  };
 
+  timerComponents(time) {
+      
+      const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+      const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+      const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+      const sec = this.pad(Math.floor((time % (1000 * 60)) / 1000));
 
-// const timer1 = document.querySelector('.timer')
-
-
-
-
-// function getTimeComponents(time){
-
-//   const days = Math.floor(time / (1000 * 60 * 60 * 24));
-//   const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//   const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-//   const secs = Math.floor((time % (1000 * 60)) / 1000);
-
-//   return {days, hours, mins, secs}
-// }
-
-function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-  
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
-    return { days, hours, minutes, seconds };
+      return {days, hours, mins, sec }
   }
-  
-  console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-  console.log(convertMs(140000)); //{days: 0, hours: 0, minutes: 2, seconds: 20}
-  console.log(convertMs(24140000)); // {days: 0, hours: 6, minutes: 42, seconds: 20}
+
+  pad(value) {
+      return String(value).padStart(2, "0");
+  };
+
+  timer() {
+      const intervalId = setInterval(() => {
+          const currentTime = Date.now();
+          const deltaTime = this.targetDate - currentTime;
+          const times = this.timerComponents(deltaTime)
+          if (deltaTime <= 0) {
+              clearInterval(intervalId);
+              return
+          }
+          const fields = [...this.selector.children];
+          fields.map((el) => {
+              const value = el.firstElementChild.dataset.value;
+               if (value === "days") {
+                  el.firstElementChild.textContent = times.days;
+              };
+               if (value === "hours") {
+                  el.firstElementChild.textContent = times.hours;
+              };
+               if (value === "mins") {
+                  el.firstElementChild.textContent = times.mins;
+              };
+               if (value === "secs") {
+                  el.firstElementChild.textContent = times.sec;
+              };
+          })
+      }, 1000)
+  }
+};
+
+
+
+
+
+
+new CountdownTimer({
+selector: '#timer-1',
+targetDate: new Date("Aug 20, 2090 10:00:00"),
+});
